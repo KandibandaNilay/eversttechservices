4517b8d38244788e8e363140703ad54361c524cb
Sandeep Konda <kondasandeep56@gmail.com>
2026-07-25 04:48:26 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  base: 'eversttechservices',  // ← replace REPO_NAME with your repo name
-  // For user site: base: '/',
+  plugins: [react(), tailwindcss()],
+  base: '/eversttechservices/', // ← exact repository name, with leading and trailing slashes
 });
